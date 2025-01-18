921403e60b500c2ff05074f4fed3fbfef579c3d5
Ricky Govan <stretchito@gmail.com>
2025-01-17 18:57:09 -0800

    Update PM2 script path to use relative path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "video-processor",
-    script: "/var/www/video-processor/dist/app.js", // Updated full path
+    script: "./dist/app.js",  // Changed to relative path
     instances: 1,
     autorestart: true,
     watch: false,
