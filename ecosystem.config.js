b02992d2f2fd5431291d3e8848ce508ba6698526
Ricky Govan <stretchito@gmail.com>
2025-01-29 12:26:05 -0800

    Update Deploy and ecosystems.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,15 +15,8 @@ module.exports = {
     log_file: 'logs/combined.log',
     time: true,
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-    node_args: [
-      '--experimental-specifier-resolution=node',
-      '--trace-warnings'
-    ],
-    source_map_support: true,
     merge_logs: true,
     log_type: 'json',
-    // Add TypeScript specific configuration
-    interpreter: 'node',
-    interpreter_args: '--require ts-node/register'
+    source_map_support: true
   }]
 };
